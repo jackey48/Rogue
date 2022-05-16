cd397f91fc0dfae17c7f537f7a3e48668e1977f1
S332847409 <102601662+S332847409@users.noreply.github.com>
2022-05-16 10:45:39 -0400

    Add files via upload

---
+++ CombatBuild May 13th/CombatBuild May 16th/src/finalproject/EnemyStats.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalproject;
+
+/**
+ *
+ * @author msull1
+ */
+public class EnemyStats extends Stats{
+    public EnemyStats(String n, int mh, int d, int a) {
+        super(n, mh, d, a);
+     }
+}
